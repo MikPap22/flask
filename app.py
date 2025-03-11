e623459b5795f5ec05af3bdb58f4c12e37393e09
MikPap22 <lon22papy01@mpw.ac.uk>
2025-03-11 12:51:18 +0000

    text files

--- app.py
+++ app.py
@@ -7,11 +7,29 @@ def login():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-        if password == "123":
+        f = open("login.txt","r")
+        username = f.readline().strip()
+        password = f.readline()
+        if username == request.form["username"] and \
+            password == request.form["password"]:
             return "Hello " + username
         else:
             return "wrong password"
+
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        f = open("login.txt", "w")
+        f.write(username + "\n")
+        f.write(password)
+        f.close()
+        return "signup success " 
+        
+
 
 
