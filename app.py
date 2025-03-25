b6e73481680a7a4d0184456680ff0f394235286e
MikPap22 <lon22papy01@mpw.ac.uk>
2025-03-25 12:14:40 +0000

    hashing

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 
+import sqlite3
+import hashlib
 app = Flask(__name__)
 
 
-import sqlite3
 con = sqlite3.connect("login.db")
 cur = con.cursor()
 cur.execute(""" CREATE TABLE IF NOT EXISTS Users ( 
@@ -14,28 +15,45 @@ con.commit()
 con.close()
 
 
-@app.route("/", methods=["GET","POST"])
-def login():
-    if request.method == "GET":
-        return render_template("index.html")
-    else:
-        if  "bob" == request.form["username"] and \
-            "123" == request.form["password"]:
-            return "Hello " + "bob"
-        else:
-            return "wrong password"
-
-
 @app.route("/signup", methods=["GET","POST"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-       
+        
+        con = sqlite3.connect("login.db")
+        cur = con.cursor()
+        hash=hashlib.sha256(request.form["password"].encode()).hexdigest()
+        cur.execute(""" INSERT INTO Users(UserName, UserPassword)
+            VALUES (?,?)""", 
+            (request.form["username"], hash))
+        con.commit()
+        con.close()
+
         return "signup success"
+
+
+
+@app.route("/", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+            con = sqlite3.connect("login.db")
+            cur = con.cursor()
+            hash=hashlib.sha256(request.form["password"].encode()).hexdigest()
+            cur.execute(" SELECT * FROM Users WHERE UserName = ? AND UserPassword = ?",
+                        (request.form["username"], hash))
+            
+            user = cur.fetchone()
+            print(user)
+            if user:
+                 return "login success"
+            else:
+                return "login failed"
+
 
 if __name__ == "__main__":
     app.run(debug = True)
+
 
